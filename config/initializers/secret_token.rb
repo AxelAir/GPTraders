d7a801cfddacca272b6927be5fa5d36651437745
Axel Air <axel.rouimi@gmail.com>
2014-09-21 17:09:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GPTraders::Application.config.secret_token = '1bbfb787e2393589191887e2308f7ba4b30a664f3bf0576b35a691c425a36408bb0acf8bf54b26402cb4029cf8e19038ab3e7d503aa6a2b7ad97465989fb7572'
